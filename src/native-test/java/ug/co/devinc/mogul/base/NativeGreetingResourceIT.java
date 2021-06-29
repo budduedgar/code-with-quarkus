c54cd02ac0230f9a4ac7475839c5ccf7b0b2dabe
quarkusio <no-reply@quarkus.io>
2021-06-29 08:57:58 +0000

    Initial commit

---
+++ src/native-test/java/ug/co/devinc/mogul/base/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ug.co.devinc.mogul.base;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
